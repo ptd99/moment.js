2722e4a78f9b64b2063441ae44006a237855d650
ptd99 <70047950+ptd99@users.noreply.github.com>
2021-03-20 21:59:48 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -13,7 +13,7 @@ function paintImage(){
     const number = Math.floor(Math.random() * IMG_NUMBER);
 
     const image = new Image();
-    image.src = `https://github.com/ptd99/moment.js/blob/main/images/img${number + 1}.jpg`
+    image.src = `https://raw.githubusercontent.com/ptd99/moment.js/main/images/${number + 1}.jpg`
     image.classList.add('bgImage');
     image.id = `img${count}`;
     count++;
