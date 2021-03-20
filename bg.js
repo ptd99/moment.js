57459816f8d2c9f21ce30aba9070ad003b9bd367
ptd99 <70047950+ptd99@users.noreply.github.com>
2021-03-20 23:54:41 +0900

    gave up making proper link to the image

--- bg.js
+++ bg.js
@@ -13,7 +13,8 @@ function paintImage(){
     const number = Math.floor(Math.random() * IMG_NUMBER);
 
     const image = new Image();
-    image.src = `./moment.js/images/img${number + 1}.jpg`;
+    image.src = `https://images.unsplash.com/photo-1468476775582-6bede20f356f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1066&q=80`;
+    // failed to load a correct path
     image.classList.add('bgImage');
     image.id = `img${count}`;
     count++;
