97b38716d51041cfb10d558cda942b93d7c8a8f4
ptd99 <70047950+ptd99@users.noreply.github.com>
2021-03-20 23:33:09 +0900

    added Pending/Done

--- todo.js
+++ todo.js
@@ -1,11 +1,54 @@
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-toDoList");
+const lists = document.querySelector(".js-lists");
+const toDoList = lists.querySelector(".js-toDoList");
+const doneList = lists.querySelector(".js-doneList");
 const TODOS_LS = 'toDos';
+const DONES_LS = 'dones';
 
 let toDos = [];
+let dones = [];
 
+function undoHandler(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    doneList.removeChild(li);
+    const originalId = parseInt(li.id);
+    const text = dones.find((x) => x.id === originalId).text;
+    const cleanDones = dones.filter(function (toDo) {
+        return toDo.id !== originalId;
+      });
 
+    dones = cleanDones;
+    paintToDo(text);
+}
+
+function doneHandler(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const originalId = parseInt(li.id);
+    const text = toDos.find((x) => x.id === originalId).text;
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== originalId;
+      });
+
+    toDos = cleanToDos;
+    paintDone(text);
+
+}
+function deleteDones(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    doneList.removeChild(li);
+    const cleanDones = dones.filter(function(toDo)
+    {
+        return toDo.id !== parseInt(li.id);
+    });
+    dones = cleanDones;
+    saveToDos();
+
+}
 function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
@@ -21,21 +64,64 @@ function deleteToDo(event){
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-
+    localStorage.setItem(DONES_LS, JSON.stringify(dones));
 }
-function paintToDo(text){
+
+function paintDone(text){
     const li = document.createElement("li");
     const dot = document.createElement("i");
+    const undoBtn = document.createElement("i");
     const delBtn = document.createElement("i");
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
+    let newId = toDos.length + 1;
+    dones.forEach(function (toDo) {
+        if (toDo.id === newId) {
+          newId = newId + 1;
+        }
+      });
 
     dot.className = "fas fa-caret-right fa-3x";
     delBtn.className = "fas fa-times";
-    delBtn.addEventListener("click",deleteToDo);
+    undoBtn.className = "fas fa-undo";
+    delBtn.addEventListener("click",deleteDones);
+    undoBtn.addEventListener("click",undoHandler);
     span.innerText = text;
     li.appendChild(dot);
     li.appendChild(span);
+    li.appendChild(undoBtn);
+    li.appendChild(delBtn);
+    li.id = newId;
+    doneList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    dones.push(toDoObj);
+    saveToDos();
+}
+
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const dot = document.createElement("i");
+    const doneBtn = document.createElement("i");
+    const delBtn = document.createElement("i");
+    const span = document.createElement("span");
+    let newId = toDos.length + 1;
+    toDos.forEach(function (toDo) {
+        if (toDo.id === newId) {
+          newId = newId + 1;
+        }
+    });
+    dot.className = "fas fa-caret-right fa-3x";
+    delBtn.className = "fas fa-times";
+    doneBtn.className = "fas fa-check";
+    delBtn.addEventListener("click",deleteToDo);
+    doneBtn.addEventListener("click",doneHandler);
+    span.innerText = text;
+    li.appendChild(dot);
+    li.appendChild(span);
+    li.appendChild(doneBtn);
     li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
@@ -57,11 +143,19 @@ function handleSubmit(event){
 
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
+    const loadedDones = localStorage.getItem(DONES_LS);
     if(loadedToDos !== null){
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         })
+    }
+    if(loadedDones !== null){
+        const parsedDones = JSON.parse(loadedDones);
+        parsedDones.forEach(function(toDo){
+            paintDone(toDo.text);
+
+        });
     }
     
 }
